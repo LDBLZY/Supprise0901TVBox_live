ea7bc16c3e94a7f6b25625c29f5deaddb31ff12c
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2024-08-05 23:17:48 +0800

    Update search_Tv.py

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -73,9 +73,9 @@ def get_url(name):
             # print(result_divs)
             # 打印提取到的 <div class="result"> 标签
             for div in result_divs:
-                print(div)
+                # print(div)
                 # 如果要获取标签内的文本内容
-                # print(etree.tostring(div, pretty_print=True).decode())
+                print(etree.tostring(div, pretty_print=True).decode())
                 for element in div.xpath(".//tba[@class='glshle']"):
                     if element.text is not None:
                         m3u8_list.append(element.text.strip())
