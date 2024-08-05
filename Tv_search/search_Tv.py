f8428cce6b8574e795a42d3cc293cdb5a7b6cc50
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2024-08-06 01:04:37 +0800

    Update search_Tv.py

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -74,7 +74,7 @@ def get_url(name):
             # 打印提取到的 <div class="result"> 标签
             for div in result_divs:
                 print('...aaa...')
-                print(etree.tostring(div, pretty_print=True))
+                print(etree.tostring(div, pretty_print=True).decode())
                 print('...nnn...')
                 # 如果要获取标签内的文本内容
                 # print(etree.tostring(div, pretty_print=True).decode())
