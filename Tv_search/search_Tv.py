e5350e39d57415adcbde12c66dda615f22870f99
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2024-08-05 21:57:55 +0800

    Update search_Tv.py

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -69,13 +69,13 @@ def get_url(name):
             # print(response.text)
             # 将 HTML 转换为 Element 对象
             root = etree.HTML(response.text)
-            result_divs = root.xpath("//div[@class='gaxgfi']")
+            result_divs = root.xpath("//div[@class='resultplus']")
             print(result_divs)
             # 打印提取到的 <div class="result"> 标签
             for div in result_divs:
                 # 如果要获取标签内的文本内容
                 # print(etree.tostring(div, pretty_print=True).decode())
-                for element in div.xpath(".//tba"):
+                for element in div.xpath(".//tba[class=glshle]"):
                     if element.text is not None:
                         m3u8_list.append(element.text.strip())
                         print(element.text.strip())
