3ae456823681b10ee5dc4ca85068f197719a96f6
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2024-08-05 21:43:58 +0800

    Update search_Tv.py

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -70,7 +70,7 @@ def get_url(name):
             # 将 HTML 转换为 Element 对象
             root = etree.HTML(response.text)
             result_divs = root.xpath("//div[@class='gaxgfi']")
-            print result_divs
+            print(result_divs)
             # 打印提取到的 <div class="result"> 标签
             for div in result_divs:
                 # 如果要获取标签内的文本内容
