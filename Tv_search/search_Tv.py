2cab90d360e7624192355e1f165cb53efd24948e
LDBLZY <91302495+LDBLZY@users.noreply.github.com>
2024-08-06 09:33:12 +0800

    Update search_Tv.py

--- Tv_search/search_Tv.py
+++ Tv_search/search_Tv.py
@@ -73,13 +73,13 @@ def get_url(name):
             
             # 打印提取到的 <div class="result"> 标签
             for div in result_divs:
-                print('...aaa...')
+                print(div)
                 # 如果要获取标签内的文本内容
                 # print(etree.tostring(div, pretty_print=True).decode())
-                for element in div:
-                    if element.text is not None:
-                        m3u8_list.append(element.text.strip())
-                        print(element.text.strip())
+                # for element in div:
+                #    if element.text is not None:
+                #        m3u8_list.append(element.text.strip())
+                #        print(element.text.strip())
         return m3u8_list
 
     except requests.exceptions.RequestException as e:
